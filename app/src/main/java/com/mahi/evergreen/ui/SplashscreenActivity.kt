2393396b05de879b0c0126698d737e9a80ab0dfc
Cuento3yLlevo2 <mhermoso1993@gmail.com>
2021-02-23 19:19:26 +0100

    create SplashScreenActivity and dimens files

---
+++ app/src/main/java/com/mahi/evergreen/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.mahi.evergreen.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahi.evergreen.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
