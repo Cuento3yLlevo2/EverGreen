53f4142302fde7dfce84cff3e145700c7ed7bdfe
Cuento3yLlevo2 <mhermoso1993@gmail.com>
2021-03-09 09:02:58 +0100

    Changed mainactivity for MainActivity2.kt and implemented a tablayout using a viwpager and create the ViewPagerAdapter. Also created fragment_chats.xml fragment_search.xml and fragment_settings.xml. Finally it was done the frist helloworld of the App

--- app/src/main/java/com/mahi/evergreen/MainActivity2.kt
+++ app/src/main/java/com/mahi/evergreen/MainActivity2.kt
@@ -1,14 +1,72 @@
 package com.mahi.evergreen
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.mahi.evergreen.ui.fragments.ChatsFragment
+import com.mahi.evergreen.ui.fragments.SearchFragment
+import com.mahi.evergreen.ui.fragments.SettingsFragment
+import kotlin.collections.ArrayList
 
 class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(findViewById(R.id.toolbar_main2))
 
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main2)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
+
+        val tabLayout: TabLayout = findViewById(R.id.tlMainTabLayout)
+        val viewPager: ViewPager = findViewById(R.id.vpMainViewPager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addFragment(ChatsFragment(), "Chats")
+        viewPagerAdapter.addFragment(SearchFragment(), "Search")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Settings")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
     }
+
+    // Adapter needed for the ViewPager and navegation
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
+        FragmentPagerAdapter(fragmentManager,  BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
+    {
+
+        private val fragments: ArrayList<Fragment>
+        private val titles: ArrayList<String>
+
+        init {
+            fragments = ArrayList<Fragment>()
+            titles = ArrayList<String>()
+        }
+
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String){
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+
+        override fun getPageTitle(i: Int): CharSequence? {
+            return titles[i]
+        }
+    }
+
 }
